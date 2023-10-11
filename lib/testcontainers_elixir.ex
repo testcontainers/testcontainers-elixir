5ab9dc229e676260c0306789bcfd2629b61a95a1
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-11 19:18:51 +0200

    use fixed version of docker-engine-api

--- lib/testcontainers_elixir.ex
+++ lib/testcontainers_elixir.ex
@@ -6,28 +6,10 @@ defmodule TestcontainersElixir do
 
   def hello do
     DockerEngineAPI.Connection.new(
-      base_url: base_url()
+      base_url: "http+unix://%2Fvar%2Frun%2Fdocker.sock/v1.43",
+      adapter: Tesla.Adapter.Hackney
     )
     |> DockerEngineAPI.Api.Image.image_list()
   end
-
-  @socket_path "unix:///var/run/docker.sock"
-  @default_version "v1.43"
-
-  defp base_url() do
-    host = Application.get_env(:docker, :host) || System.get_env("DOCKER_HOST", @socket_path)
-    version =
-      case Application.get_env(:docker, :version) do
-        nil -> @default_version
-        version -> version
-      end
-
-
-    "#{normalize_host(host)}/#{version}"
-    |> String.trim_trailing("/")
-  end
-
-  defp normalize_host("tcp://" <> host), do: "http://" <> host
-  defp normalize_host("unix://" <> host), do: "http+unix://" <> URI.encode_www_form(host)
 
 end
