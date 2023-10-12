f177ba848e1b7d9d2ea79bf7723012def639211f
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-12 20:35:43 +0200

    rename test

--- test/simple_test.exs
+++ test/simple_test.exs
@@ -2,7 +2,7 @@ defmodule SimpleTest do
   use ExUnit.Case
   import TestcontainersElixir.ExUnit
 
-  test "creates and reapes container" do
+  test "creates and reaps container" do
     {:ok, container_id} = container(image: "nginx:latest", port: 80)
     assert is_binary(container_id)
   end
