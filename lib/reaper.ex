6ac79df9ecbc3f8773ea677f28c62244db22ce4a
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-12 18:32:02 +0200

    send filters to ryuk

--- lib/reaper.ex
+++ lib/reaper.ex
@@ -6,22 +6,27 @@ defmodule TestcontainersElixir.Reaper do
   @ryuk_port 8080
 
   def start_link(connection) do
-    GenServer.start_link(__MODULE__, connection)
+    GenServer.start_link(__MODULE__, connection, name: __MODULE__)
   end
 
+  def register(filter) do
+    GenServer.call(__MODULE__, {:register, filter})
+  end
+
+  @impl true
   def init(connection) do
     {:ok, _} =
       connection
       |> DockerEngineAPI.Api.Image.image_create(fromImage: @ryuk_image)
 
-    {:ok, container} =
+    {:ok, %DockerEngineAPI.Model.ContainerCreateResponse{Id: container_id} = container} =
       connection
       |> DockerEngineAPI.Api.Container.container_create(
         %DockerEngineAPI.Model.ContainerCreateRequest{
           Image: @ryuk_image,
           ExposedPorts: %{"#{@ryuk_port}" => %{}},
           HostConfig: %{
-            PortBindings: %{"8080" => [%{"HostPort" => ""}]},
+            PortBindings: %{"#{@ryuk_port}" => [%{"HostPort" => ""}]},
             Privileged: true,
             Binds: ["/var/run/docker.sock:/var/run/docker.sock:rw"]
           },
@@ -29,13 +34,50 @@ defmodule TestcontainersElixir.Reaper do
         }
       )
 
-    {:ok, container} =
+    {:ok, _} =
       connection
-      |> DockerEngineAPI.Api.Container.container_start(container."Id")
+      |> DockerEngineAPI.Api.Container.container_start(container_id)
 
-    # TODO establish socket connection
-    # TODO send first message
-    # TODO return {:ok, {container, socket}} for ex
-    {:ok, container}
+    {:ok, socket} =
+      connection
+      |> connect_to_first_tcp_port(container, @ryuk_port)
+
+    {:ok, {container, socket}}
   end
+
+  @impl true
+  def handle_call({:register, filter}, _from, {_connection, socket} = state) do
+    {:reply, do_register(socket, filter), state}
+  end
+
+  defp do_register(socket, {filter_key, filter_value}) do
+    :gen_tcp.send(socket, (docker_filter(filter_key, filter_value) <> "\n") |> IO.inspect())
+    wait_for_ack(socket)
+    :ok
+  end
+
+  defp connect_to_first_tcp_port(
+         connection,
+         %DockerEngineAPI.Model.ContainerCreateResponse{Id: container_id},
+         port
+       ) do
+    port_str = "#{port}/tcp"
+
+    {:ok,
+     %DockerEngineAPI.Model.ContainerInspectResponse{
+       NetworkSettings: %{Ports: %{^port_str => [%{"HostPort" => host_port}]}}
+     }} = connection |> DockerEngineAPI.Api.Container.container_inspect(container_id)
+
+    :gen_tcp.connect(~c"localhost", String.to_integer(host_port), [
+      :binary,
+      active: false,
+      packet: :line
+    ])
+  end
+
+  defp docker_filter(key, value), do: "#{url_encode(key)}=#{url_encode(value)}"
+
+  defp url_encode(string), do: :uri_string.quote(string)
+
+  defp wait_for_ack(socket), do: {:ok, "ACK\n"} = :gen_tcp.recv(socket, 0, 1_000)
 end
--- lib/testcontainers_elixir.ex
+++ lib/testcontainers_elixir.ex
@@ -9,6 +9,7 @@ defmodule TestcontainersElixir do
       DockerEngineAPI.Connection.new(base_url: "http+unix://%2Fvar%2Frun%2Fdocker.sock/v1.43")
 
     {:ok, _pid} = connection |> TestcontainersElixir.Reaper.start_link()
+    :ok = TestcontainersElixir.Reaper.register({"id", "some_container_id?"})
     connection |> DockerEngineAPI.Api.Image.image_list()
   end
 end
