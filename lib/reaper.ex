bb84d89477ff4507b83e2dd40ef3127d04c3ff54
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-12 17:47:24 +0200

    make test pass with reaper

--- lib/reaper.ex
+++ lib/reaper.ex
@@ -2,17 +2,24 @@
 defmodule TestcontainersElixir.Reaper do
   use GenServer
 
+  @ryuk_image "testcontainers/ryuk:0.5.1"
+  @ryuk_port 8080
+
   def start_link(connection) do
     GenServer.start_link(__MODULE__, connection)
   end
 
   def init(connection) do
+    {:ok, _} =
+      connection
+      |> DockerEngineAPI.Api.Image.image_create(fromImage: @ryuk_image)
+
     {:ok, container} =
       connection
       |> DockerEngineAPI.Api.Container.container_create(
         %DockerEngineAPI.Model.ContainerCreateRequest{
-          Image: "testcontainers/ryuk:0.5.1",
-          ExposedPorts: %{"8080" => %{}},
+          Image: @ryuk_image,
+          ExposedPorts: %{"#{@ryuk_port}" => %{}},
           HostConfig: %{
             PortBindings: %{"8080" => [%{"HostPort" => ""}]},
             Privileged: true,
@@ -23,7 +30,8 @@ defmodule TestcontainersElixir.Reaper do
       )
 
     {:ok, container} =
-      connection |> DockerEngineAPI.Api.Container.container_start(container."Id")
+      connection
+      |> DockerEngineAPI.Api.Container.container_start(container."Id")
 
     # TODO establish socket connection
     # TODO send first message
--- lib/testcontainers_elixir.ex
+++ lib/testcontainers_elixir.ex
@@ -7,6 +7,7 @@ defmodule TestcontainersElixir do
   def hello do
     connection =
       DockerEngineAPI.Connection.new(base_url: "http+unix://%2Fvar%2Frun%2Fdocker.sock/v1.43")
+
     {:ok, _pid} = connection |> TestcontainersElixir.Reaper.start_link()
     connection |> DockerEngineAPI.Api.Image.image_list()
   end
