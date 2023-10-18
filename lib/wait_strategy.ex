0fc721c1d3c88ea3467bb11e9223d3e8e57de66a
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-18 09:03:42 +0200

    improve doc

--- lib/wait_strategy.ex
+++ lib/wait_strategy.ex
@@ -2,7 +2,9 @@
 # Original by: Marco Dallagiacoma @ 2023 in https://github.com/dallagi/excontainers
 # Modified by: Jarl André Hübenthal @ 2023
 defprotocol Testcontainers.WaitStrategy do
-  @moduledoc false
+  @moduledoc """
+  Defines the protocol/interface for the wait strategies in `Testcontainers`
+  """
 
   @spec wait_until_container_is_ready(t, String.t()) :: :ok | {:error, atom()}
   def wait_until_container_is_ready(wait_strategy, id_or_name)
