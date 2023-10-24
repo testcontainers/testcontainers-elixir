ffbc0a0cde3b3d0c1a067c3e144762e89643a4f3
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-24 19:26:18 +0200

    add simple test to increase test coverage

---
+++ test/constants_test.exs
@@ -0,0 +1,11 @@
+defmodule Testcontainers.ConstantsTest do
+  use ExUnit.Case, async: true
+
+  test "have correct values" do
+    assert Testcontainers.Constants.container_label() == "org.testcontainers"
+    assert Testcontainers.Constants.container_sessionId_label() == "org.testcontainers.sessionId"
+    assert Testcontainers.Constants.container_version_label() == "org.testcontainers.version"
+    assert is_binary(Testcontainers.Constants.library_version())
+    assert is_atom(Testcontainers.Constants.library_name())
+  end
+end
