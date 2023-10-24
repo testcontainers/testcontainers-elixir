886051caf32b9004b81d29bbb297d230bf6a7e75
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-24 20:38:28 +0200

    run specific test for it self

--- other_tests/testcontainers_test.exs
+++ other_tests/testcontainers_test.exs
@@ -3,11 +3,13 @@ defmodule TestcontainersTest do
   alias Testcontainers.Container
   use ExUnit.Case, async: true
 
+  @moduletag timeout: 300_000
+
   test "will cleanup containers" do
     {:ok, container} = Container.run(MySqlContainer.new())
     GenServer.stop(Testcontainers)
     :timer.sleep(15_000)
-    {:ok, pid} = Testcontainers.start_link()
+    {:ok, _} = Testcontainers.start_link()
     {:error, _} = Testcontainers.get_container(container.container_id)
   end
 end
