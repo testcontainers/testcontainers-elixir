0646714aaf648e1e838db31f98324e7aa9b235e6
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-29 19:12:04 +0100

    refactor

--- lib/wait_strategy/log_wait_strategy.ex
+++ lib/wait_strategy/log_wait_strategy.ex
@@ -5,77 +5,69 @@ defmodule Testcontainers.WaitStrategy.LogWaitStrategy do
   """
 
   @retry_delay 500
-
   defstruct [:log_regex, :timeout, retry_delay: @retry_delay]
 
+  # Public interface
+
   @doc """
-  Creates a new LogWaitStrategy to wait until a specific log message, matching the provided regex, appears in the container's log.
+  Creates a new LogWaitStrategy.
+  This strategy waits until a specific log message, matching the provided regex, appears in the container's log.
   """
-  def new(log_regex, timeout \\ 5000, retry_delay \\ @retry_delay),
-    do: %__MODULE__{log_regex: log_regex, timeout: timeout, retry_delay: retry_delay}
+  def new(log_regex, timeout \\ 5000, retry_delay \\ @retry_delay) do
+    %__MODULE__{log_regex: log_regex, timeout: timeout, retry_delay: retry_delay}
+  end
+
+  # Private functions and implementations
 
   defimpl Testcontainers.WaitStrategy do
-    alias Testcontainers.Docker
-    alias Testcontainers.Container
-    alias Testcontainers.WaitStrategy.LogWaitStrategy
-    alias Testcontainers.Logger
+    alias Testcontainers.{Docker, Logger}
 
-    def wait_until_container_is_ready(
-          %LogWaitStrategy{} = wait_strategy,
-          %Container{} = container,
-          conn
-        ) do
-      case wait_for_log(
-             wait_strategy,
-             container.container_id,
-             conn,
-             current_time_millis()
-           ) do
-        {:ok, :log_is_ready} ->
+    @impl true
+    def wait_until_container_is_ready(wait_strategy, container, conn) do
+      started_at = get_current_time_millis()
+      wait_for_log_message(wait_strategy, container.container_id, conn, started_at)
+    end
+
+    # Main loop for waiting strategy
+    defp wait_for_log_message(wait_strategy, container_id, conn, start_time) do
+      if reached_timeout?(start_time, wait_strategy.timeout) do
+        {:error, strategy_timed_out(wait_strategy.timeout, start_time), wait_strategy}
+      else
+        process_log(wait_strategy, container_id, conn, start_time)
+      end
+    end
+
+    defp process_log(wait_strategy, container_id, conn, start_time) do
+      case log_matches?(container_id, wait_strategy.log_regex, conn) do
+        true ->
           :ok
 
-        {:error, reason} ->
-          {:error, reason, wait_strategy}
+        false ->
+          log_retry_message(container_id, wait_strategy.log_regex, wait_strategy.retry_delay)
+          :timer.sleep(wait_strategy.retry_delay)
+          wait_for_log_message(wait_strategy, container_id, conn, start_time)
       end
     end
 
-    defp wait_for_log(%LogWaitStrategy{} = wait_strategy, container_id, conn, start_time)
-         when is_binary(container_id) and is_integer(wait_strategy.timeout) and
-                is_integer(start_time) do
-      if wait_strategy.timeout + start_time < current_time_millis() do
-        {:error, strategy_timed_out(wait_strategy.timeout, start_time)}
-      else
-        if log_comparison(container_id, wait_strategy.log_regex, conn) do
-          {:ok, :log_is_ready}
-        else
-          delay = max(0, wait_strategy.retry_delay)
-
-          Logger.log(
-            "Logs in container #{container_id} didnt match regex #{inspect(wait_strategy.log_regex)}, retrying in #{delay}ms."
-          )
-
-          :timer.sleep(delay)
-          wait_for_log(wait_strategy, container_id, conn, start_time)
-        end
-      end
+    defp log_matches?(container_id, log_regex, conn) do
+      with {:ok, log_output} <- Docker.Api.stdout_logs(container_id, conn),
+           do: Regex.match?(log_regex, log_output)
     end
 
-    defp log_comparison(container_id, log_regex, conn) do
-      case Docker.Api.stdout_logs(container_id, conn) do
-        {:ok, stdout_log} ->
-          Regex.match?(log_regex, stdout_log)
+    defp get_current_time_millis(), do: System.monotonic_time(:millisecond)
 
-        {:error, _} ->
-          false
-      end
+    defp reached_timeout?(start_time, timeout),
+      do: get_current_time_millis() - start_time > timeout
+
+    defp strategy_timed_out(timeout, start_time) do
+      {:log_wait_strategy, :timeout, timeout,
+       elapsed_time: get_current_time_millis() - start_time}
     end
 
-    defp current_time_millis, do: System.monotonic_time(:millisecond)
-
-    defp strategy_timed_out(timeout, started_at)
-         when is_number(timeout) and is_number(started_at),
-         do:
-           {:log_wait_strategy, :timeout, timeout,
-            elapsed_time: current_time_millis() - started_at}
+    defp log_retry_message(container_id, log_regex, delay) do
+      Logger.log(
+        "Logs in container #{container_id} didn't match regex #{inspect(log_regex)}, retrying in #{delay}ms."
+      )
+    end
   end
 end
