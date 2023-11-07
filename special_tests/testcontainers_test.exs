56f0bfb3b5d3b7ae7594ac6520cbc584b9178444
Jarl André Hübenthal <jarlah@protonmail.com>
2023-11-07 15:12:42 +0100

    fix test

--- special_tests/testcontainers_test.exs
+++ special_tests/testcontainers_test.exs
@@ -11,8 +11,19 @@ defmodule TestcontainersTest do
     GenServer.stop(Testcontainers)
     TestHelper.wait_for_genserver_state(Testcontainers, :down)
     {:ok, _} = Testcontainers.start_link()
-    :ok = TestHelper.wait_for_lambda(fn -> with {:error, _} <-
-      Docker.Api.get_container(container.container_id, Connection.get_connection()), do: :ok
-    end, max_retries: 15, interval: 1000)
+
+    :ok =
+      TestHelper.wait_for_lambda(
+        fn ->
+          with {:error, _} <-
+                 Docker.Api.get_container(
+                   container.container_id,
+                   Connection.get_connection() |> Tuple.to_list() |> Kernel.hd()
+                 ),
+               do: :ok
+        end,
+        max_retries: 15,
+        interval: 1000
+      )
   end
 end
