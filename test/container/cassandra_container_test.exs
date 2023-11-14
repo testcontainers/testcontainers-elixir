327e21d25c005b9229aef475989478b7a4e5aaad
Jarl André Hübenthal <jarlah@protonmail.com>
2023-11-14 12:11:54 +0100

    add proper test

--- test/container/cassandra_container_test.exs
+++ test/container/cassandra_container_test.exs
@@ -19,8 +19,37 @@ defmodule Testcontainers.Container.CassandraContainerTest do
     container(:cassandra, @cassandra_container)
 
     test "can create and start", %{cassandra: cassandra} do
-      {:ok, _conn} = Xandra.start_link(nodes: [CassandraContainer.connection_uri(cassandra)])
-      # TODO
+      {:ok, conn} = Xandra.start_link(nodes: [CassandraContainer.connection_uri(cassandra)])
+
+      {:ok, _result} =
+        Xandra.execute(
+          conn,
+          "CREATE KEYSPACE cassandra_test WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3}"
+        )
+
+      {:ok, _result} =
+        Xandra.execute(
+          conn,
+          "CREATE TABLE cassandra_test.users (id int PRIMARY KEY, name varchar)"
+        )
+
+      {:ok, _result} =
+        Xandra.execute(
+          conn,
+          "INSERT INTO cassandra_test.users(id, name) VALUES(1, 'Very special name')"
+        )
+
+      page =
+        with {:ok, prepared} <-
+               Xandra.prepare(
+                 conn,
+                 "SELECT * FROM cassandra_test.users WHERE name = ? ALLOW FILTERING"
+               ),
+             {:ok, %Xandra.Page{} = page} <-
+               Xandra.execute(conn, prepared, [_name = "Very special name"]),
+             do: page
+
+      assert page.content |> Kernel.hd() |> List.last() == "Very special name"
     end
   end
 end
