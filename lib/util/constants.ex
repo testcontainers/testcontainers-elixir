8d4ed55ff624e13b289883d34b3d7eeda82258f1
Jarl André Hübenthal <jarlah@protonmail.com>
2023-12-01 07:19:15 +0100

    v1.5.0

--- lib/util/constants.ex
+++ lib/util/constants.ex
@@ -2,7 +2,7 @@ defmodule Testcontainers.Constants do
   @moduledoc false
 
   def library_name, do: :testcontainers
-  def library_version, do: "1.4.2"
+  def library_version, do: "1.5.0"
   def container_label, do: "org.testcontainers"
   def container_lang_label, do: "org.testcontainers.lang"
   def container_lang_value, do: Elixir |> Atom.to_string() |> String.downcase()
