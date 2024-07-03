f691792ef2bf740849623a4530f0f8b8d6e3903d
Adriano Santos <solid.sistemas@gmail.com>
2024-07-03 01:19:45 -0300

    Add support to network host mode (#107)
    * Add support to network host mode
    
    * Add reusable test helper function open_port?

--- test/generic_container_test.exs
+++ test/generic_container_test.exs
@@ -1,9 +1,19 @@
 defmodule Testcontainers.GenericContainerTest do
   use ExUnit.Case, async: true
 
+  import TestHelper, only: [port_open?: 2]
+
   test "can start and stop generic container" do
     config = %Testcontainers.Container{image: "redis:latest"}
     assert {:ok, container} = Testcontainers.start_container(config)
+    assert :ok = Testcontainers.stop_container(container.container_id)
+  end
+
+  test "can start and stop generic container with network mode set to host" do
+    config = %Testcontainers.Container{image: "redis:latest", network_mode: "host"}
+    assert {:ok, container} = Testcontainers.start_container(config)
+    Process.sleep(5000)
+    assert :ok = port_open?("127.0.0.1", 6379)
     assert :ok = Testcontainers.stop_container(container.container_id)
   end
 end
