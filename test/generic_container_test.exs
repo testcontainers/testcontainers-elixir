d73fb12082849224cc3ade3f6565fd411a0625dc
Jarl André Hübenthal <jarlah@protonmail.com>
2024-07-08 21:38:03 +0200

    fix: log warning when using network_mode=host on non-linux hosts (#115)

--- test/generic_container_test.exs
+++ test/generic_container_test.exs
@@ -1,6 +1,7 @@
 defmodule Testcontainers.GenericContainerTest do
   use ExUnit.Case, async: true
 
+  import Testcontainers.Container, only: [is_os: 1]
   import TestHelper, only: [port_open?: 2]
 
   test "can start and stop generic container" do
@@ -10,13 +11,14 @@ defmodule Testcontainers.GenericContainerTest do
   end
 
   test "can start and stop generic container with network mode set to host" do
-    config = %Testcontainers.Container{image: "redis:latest", network_mode: "host"}
-    assert {:ok, container} = Testcontainers.start_container(config)
-    Process.sleep(5000)
-    with {:unix, :darwin} <- :os.type() do
-      IO.puts("Testing network_mode=host doesn't work in macos!")
+    if not is_os(:linux) do
+      Testcontainers.Logger.log("Host is not Linux, therefore not running network_mode test")
+    else
+      config = %Testcontainers.Container{image: "redis:latest", network_mode: "host"}
+      assert {:ok, container} = Testcontainers.start_container(config)
+      Process.sleep(5000)
+      assert :ok = port_open?("127.0.0.1", 6379)
+      assert :ok = Testcontainers.stop_container(container.container_id)
     end
-    assert :ok = port_open?("127.0.0.1", 6379)
-    assert :ok = Testcontainers.stop_container(container.container_id)
   end
 end
