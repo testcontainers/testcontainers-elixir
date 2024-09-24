4362bd3fa65499e390dbc80e44f13aeb8d927456
Jarl André Hübenthal <jarlah@protonmail.com>
2024-09-24 08:00:44 +0200

    change labels

--- lib/util/constants.ex
+++ lib/util/constants.ex
@@ -5,9 +5,9 @@ defmodule Testcontainers.Constants do
   def library_version, do: "1.9.0"
   def container_label, do: "org.testcontainers"
   def container_lang_label, do: "org.testcontainers.lang"
-  def container_hash_label, do: "org.testcontainers.hash"
+  def container_hash_label, do: "org.testcontainers.reuse-hash"
   def container_lang_value, do: Elixir |> Atom.to_string() |> String.downcase()
-  def container_sessionId_label, do: "org.testcontainers.sessionId"
+  def container_sessionId_label, do: "org.testcontainers.session-id"
   def container_version_label, do: "org.testcontainers.version"
   def user_agent, do: "tc-elixir/" <> __MODULE__.library_version()
 end
