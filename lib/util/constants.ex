e5c6a19654aa21458ff58656698f840e72e36dab
Jarl André Hübenthal <jarlah@protonmail.com>
2024-09-24 13:18:23 +0200

    update ryuk version, fixes #118

--- lib/util/constants.ex
+++ lib/util/constants.ex
@@ -2,7 +2,7 @@ defmodule Testcontainers.Constants do
   @moduledoc false
 
   def library_name, do: :testcontainers
-  def library_version, do: "1.10.0"
+  def library_version, do: "1.10.1"
   def container_label, do: "org.testcontainers"
   def container_lang_label, do: "org.testcontainers.lang"
   def container_reuse_hash_label, do: "org.testcontainers.reuse-hash"
