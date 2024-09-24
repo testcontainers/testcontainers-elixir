d6040e5bcc0f775a3ef4d4c259564e94f8d203ce
Jarl André Hübenthal <jarlah@protonmail.com>
2024-09-24 11:07:08 +0200

    try nestru for struct to map transform

--- lib/util/hash.ex
+++ lib/util/hash.ex
@@ -3,7 +3,6 @@ defmodule Testcontainers.Util.Hash do
     struct
     |> Nestru.encode!()
     |> Jason.encode!()
-    |> IO.inspect()
     |> (&:crypto.hash(:sha256, &1)).()
     |> Base.encode16(case: :lower)
   end
@@ -12,5 +11,18 @@ end
 defimpl Nestru.Encoder, for: Regex do
   def gather_fields_from_struct(regex, _context) do
     {:ok, Regex.source(regex)}
+  end
+end
+
+defimpl Nestru.Encoder, for: Tuple do
+  @spec gather_fields_from_struct(tuple(), any()) :: {:ok, binary()}
+  def gather_fields_from_struct(tuple, _context) do
+    {:ok, inspect(tuple)}
+  end
+end
+
+defimpl Jason.Encoder, for: Tuple do
+  def encode(tuple, opts) do
+    Jason.Encode.string(inspect(tuple), opts)
   end
 end
