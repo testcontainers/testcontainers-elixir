33b9f5d0a82229ddd18345164ab8f4bdfe30c951
Jarl André Hübenthal <jarlah@protonmail.com>
2024-10-03 21:14:24 +0200

    improve gateway test

--- test/util/gateway_test.exs
+++ test/util/gateway_test.exs
@@ -4,9 +4,15 @@ defmodule Testcontainers.GatewayTest do
   alias Testcontainers.Connection
   import Testcontainers.GatewayUtil
 
-  test "will get gateway address" do
+  test "will get gateway address if in docker environment" do
     {conn, docker_host_url} = Connection.get_connection()
     {:ok, gateway_address} = get_docker_host(docker_host_url, conn, "test/util/.dockerenv")
     assert gateway_address != "localhost"
+  end
+
+  test "will get gateway address if not in docker environment" do
+    {conn, docker_host_url} = Connection.get_connection()
+    {:ok, gateway_address} = get_docker_host(docker_host_url, conn, "foobar/doesnotexist")
+    assert gateway_address == "localhost"
   end
 end
