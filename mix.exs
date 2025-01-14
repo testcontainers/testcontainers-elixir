4102ca322964a636e07aab7298ef97ab66d7f705
Jarl André Hübenthal <jarlah@protonmail.com>
2025-01-14 19:58:40 +0100

    v1.11.8

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TestcontainersElixir.MixProject do
   use Mix.Project
 
   @app :testcontainers
-  @version "1.11.7"
+  @version "1.11.8"
   @source_url "https://github.com/testcontainers/testcontainers-elixir"
 
   def project do
