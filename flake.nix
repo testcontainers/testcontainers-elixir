df0c4e9f1b6cfc58c74cf292f17d74b8b2869122
Jarl André Hübenthal <jarlah@protonmail.com>
2025-05-11 09:13:02 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,42 +1,91 @@
 {
-  description = "Elixir 1.17 development environment with VSCode";
+  description = "A flake template for Phoenix 1.7 projects.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    overlay = prev: final: rec {
+      # https://github.com/erlang/otp/security/advisories/GHSA-37cp-fgq5-7wc2
+      erlang = prev.beam.interpreters.erlang_27.override {
+        version = "27.3.3";
+        sha256 = "sha256-OTCCfVeJADxKlmgk8rRE3uzY8Y9qYwY/ubiopWG/0ao=";
+      };
+      beamPackages = prev.beam.packagesWith erlang;
+      elixir = beamPackages.elixir_1_18;
+      hex = beamPackages.hex;
+      final.mix2nix = prev.mix2nix.overrideAttrs {
+        nativeBuildInputs = [final.elixir];
+        buildInputs = [final.erlang];
+      };
+    };
 
-        # Define the VSCode package with desired extensions
-        vscode = pkgs.vscode-with-extensions.override {
-          vscodeExtensions = with pkgs.vscode-extensions; [
-            elixir-lsp.vscode-elixir-ls
-            phoenixframework.phoenix
-            jnoortheen.nix-ide
-            eamodio.gitlens
-          ];
-        };
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.beam.packages.erlang.elixir_1_17
-            pkgs.beam.interpreters.erlang
-            pkgs.nodejs
-            vscode
-          ];
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
 
-          shellHook = ''
-            export MIX_HOME=$PWD/.mix
-            export HEX_HOME=$PWD/.hex
-            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
-          '';
-        };
-      });
+    nixpkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [overlay];
+      };
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor system;
+      # FIXME: import the Mix deps into Nix by running `mix2nix > deps.nix` from a dev shell
+      # mixNixDeps = import ./deps.nix {
+      #  lib = pkgs.lib;
+      #  beamPackages = pkgs.beamPackages;
+      #};
+    in {
+      default = pkgs.beamPackages.mixRelease {
+        pname = "my-phx-app";
+        # Elixir app source path
+        src = ./.;
+        version = "0.1.0";
+        # FIXME: mixNixDeps was specified in the FIXME above. Uncomment the next line.
+        # inherit mixNixDeps;
+
+        # add esbuild and tailwindcss
+        buildInputs = [pkgs.elixir pkgs.esbuild pkgs.tailwindcss];
+
+        # Explicitly declare tailwind and esbuild binary paths (don't let Mix fetch them)
+        preConfigure = ''
+          substituteInPlace config/config.exs \
+            --replace "config :tailwind," "config :tailwind, path: \"${pkgs.tailwindcss}/bin/tailwindcss\","\
+            --replace "config :esbuild," "config :esbuild, path: \"${pkgs.esbuild}/bin/esbuild\", "
+        '';
+
+        # Deploy assets before creating release
+        preInstall = ''
+          # https://github.com/phoenixframework/phoenix/issues/2690
+           mix do deps.loadpaths --no-deps-check, assets.deploy
+        '';
+      };
+    });
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor system;
+    in {
+      default = self.devShells.${system}.dev;
+      dev = pkgs.callPackage ./shell.nix {
+        dbName = "db_dev";
+        mixEnv = "dev";
+      };
+      test = pkgs.callPackage ./shell.nix {
+        dbName = "db_test";
+        mixEnv = "test";
+      };
+      prod = pkgs.callPackage ./shell.nix {
+        dbName = "db_prod";
+        mixEnv = "prod";
+      };
+    });
+  };
 }
---
+++ shell.nix
@@ -0,0 +1,51 @@
+{
+  pkgs,
+  dbName,
+  mixEnv,
+  beamPackages,
+}: let
+  # define packages to install
+  basePackages = with pkgs; [
+    elixir
+    hex
+    mix2nix
+    postgresql
+    esbuild
+    tailwindcss
+  ];
+
+  # Add basePackages + optional system packages per system
+  inputs = with pkgs;
+    basePackages
+    ++ lib.optionals stdenv.isLinux [inotify-tools]
+    ++ lib.optionals stdenv.isDarwin
+    (with darwin.apple_sdk.frameworks; [CoreFoundation CoreServices]);
+
+  # define shell startup command
+  hooks = ''
+    # this allows mix to work on the local directory
+    mkdir -p .nix-mix .nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-mix
+    export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+
+    export MIX_ENV=${mixEnv}
+
+    export LANG=en_US.UTF-8
+    # keep your shell history in iex
+    export ERL_AFLAGS="-kernel shell_history enabled"
+
+    # postgres related
+    # keep all your db data in a folder inside the project
+    export PGDATA="$PWD/db"
+
+    # phoenix related env vars
+    export POOL_SIZE=15
+    export DB_URL="postgresql://postgres:postgres@localhost:5432/${dbName}"
+    export PORT=4000
+  '';
+in
+  pkgs.mkShell {
+    buildInputs = inputs;
+    shellHook = hooks;
+  }
