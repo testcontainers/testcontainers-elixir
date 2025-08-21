b4875e3a6988020a27b72f56dc85537f39154df2
Jarl André Hübenthal <jarlah@protonmail.com>
2025-08-21 07:38:43 +0200

    v1.13.2
    * Update mix.exs
    
    * Update constants.ex

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TestcontainersElixir.MixProject do
   use Mix.Project
 
   @app :testcontainers
-  @version "1.13.1"
+  @version "1.13.2"
   @source_url "https://github.com/testcontainers/testcontainers-elixir"
 
   def project do
