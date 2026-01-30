2bbb3eadcf7ce94324cc46132ab38bbe03e092e3
Jarl André Hübenthal <jarlah@protonmail.com>
2026-01-30 20:35:04 +0100

    further simplify running tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,10 +1,3 @@
-Path.wildcard("test/support/**/*.ex")
-|> Kernel.ParallelCompiler.compile()
-
 Testcontainers.start_link()
 
 ExUnit.start()
-
-# Application.put_env(:tesla, DockerEngineAPI.Connection,
-#   middleware: [{Tesla.Middleware.Logger, log_level: :info}]
-# )
